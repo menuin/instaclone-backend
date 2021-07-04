a960b35fb5516023b45439435d32d605c8ad43a9
menuin <menuin@hanyang.ac.kr>
2021-07-05 03:38:10 +0900

    #4.3 seeProfile

--- users/users.queries.js
+++ users/users.queries.js
@@ -1,5 +1,11 @@
+import client from "../client";
+
 export default {
     Query : {
-        
+        seeProfile : (_, {username}) => client.user.findUnique({
+            where  : {
+                username,
+            }
+        })
     }
 }
