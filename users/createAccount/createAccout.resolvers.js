5336a47a59bf1cbf4532269b7fe3c48419fc4354
menuin <menuin@hanyang.ac.kr>
2021-07-05 22:48:58 +0900

    #4.6 Divide and Conquer - 폴더로 분리 완료

--- users/createAccount/createAccout.resolvers.js
+++ users/createAccount/createAccout.resolvers.js
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
-import client from "../client";
-import jwt from "jsonwebtoken";
+import client from "../../client";
 
 export default {
     Mutation : {
@@ -40,30 +39,5 @@ export default {
         }
         },
 
-        login : async (_, {username, password} ) => {
-            // find user with args.username
-            const user = await client.user.findFirst({where:{username}});
-            if (!user){
-                return {
-                    ok: false,
-                    error : "User not found", 
-                }
-            }
-
-            // check password with args.password
-            const passwordOk = await bcrypt.compare(password, user.password)
-            if (!passwordOk) {
-                return  {
-                    ok : false,
-                    error : "Incorrect password",
-                }
-            }
-            // issue a token and send it to user
-            const token = await jwt.sign({id:user.id}, process.env.SECRET_KEY);
-            return {
-                ok:true,
-                token : token,
-            }
-        }
     }
 }
