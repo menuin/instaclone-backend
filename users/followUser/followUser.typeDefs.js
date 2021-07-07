5b7b77b57e254a4b72adba5c7e1ad95e8d95df4a
menuin <menuin@hanyang.ac.kr>
2021-07-08 02:26:07 +0900

    #4.21 Following on graphQL

---
+++ users/followUser/followUser.typeDefs.js
@@ -0,0 +1,14 @@
+import { gql } from "apollo-server";
+
+
+export default gql`
+
+type FollowUserResult {
+    ok: Boolean!
+    error : String
+}
+
+type Mutation {
+   followUser(username:String): FollowUserResult 
+}
+`
