672765ecb2e1d33875df80d095903151ed8af034
menuin <menuin@hanyang.ac.kr>
2021-07-16 02:46:48 +0900

    ~#7.3 sendMessage

---
+++ messages/seeRooms/seeRooms.resolvers.js
@@ -0,0 +1,17 @@
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+    Query: {
+        seeRooms: protectedResolver(async (_, __, { loggedInUser }) =>
+            client.room.findMany({
+                where: {
+                    users: {
+                        some: {
+                            id: loggedInUser.id,
+                        }
+                    }
+                }
+            })
+        )
+    }
+}
