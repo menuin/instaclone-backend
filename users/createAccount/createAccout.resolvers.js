5ba3b8b1b0847432556f0a79f0eb6726b847cd33
menuin <menuin@hanyang.ac.kr>
2021-07-24 16:29:59 +0900

    createAccount 오류 수정

--- users/createAccount/createAccout.resolvers.js
+++ users/createAccount/createAccout.resolvers.js
@@ -2,23 +2,23 @@ import bcrypt from "bcrypt";
 import client from "../../client";
 
 export default {
-    Mutation : {
-        createAccount : async (_, { firstName,lastName,username,email,password}) => {
-            try{
+    Mutation: {
+        createAccount: async (_, { firstName, lastName, username, email, password }) => {
+            try {
                 // STEP 1 : check if username or email are already on db
                 const existingUser = await client.user.findFirst({
-                    where : {
-                            OR : [
-                                {
-                                    username : username,
-                                },
-                                {
-                                    email,
-                                },
-                            ],
+                    where: {
+                        OR: [
+                            {
+                                username: username,
+                            },
+                            {
+                                email,
+                            },
+                        ],
                     },
                 });
-                if (existingUser){
+                if (existingUser) {
                     throw new Error("This username/email is already taken.");
                 }
 
@@ -27,19 +27,25 @@ export default {
                 const uglyPassword = await bcrypt.hash(password, 10);
 
                 // STEP 3 : save and return the user
-                return client.user.create({ data : {
-                    username,
-                    email, 
-                    firstName, 
-                    lastName, 
-                    password : uglyPassword,
-                }})
-        } catch (e) {
-            return {
-                ok:false,
-                error:"Can't create account"
+                await client.user.create({
+                    data: {
+                        username,
+                        email,
+                        firstName,
+                        lastName,
+                        password: uglyPassword,
+                    }
+                })
+                return {
+                    ok: true
+                }
+
+            } catch (e) {
+                return {
+                    ok: false,
+                    error: "Can't create account"
+                }
             }
-        }
         },
 
     }
