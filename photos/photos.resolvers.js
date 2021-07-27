f94757fefbdd22bb2f53b3fff4592977c0ab769f
menuin <menuin@hanyang.ac.kr>
2021-07-28 03:36:27 +0900

    update from frontend - isMine, isLiked

--- photos/photos.resolvers.js
+++ photos/photos.resolvers.js
@@ -29,6 +29,27 @@ export default {
                 return false;
             }
             return userId === loggedInUser.id;
+        },
+        isLiked: async ({ id }, _, { loggedInUser }) => {
+            if (!loggedInUser) {
+                return false;
+            }
+            const ok = await client.like.findUnique({
+                where: {
+                    photoId_userId: { // compound unique
+                        photoId: id,
+                        userId: loggedInUser.id,
+                    }
+                },
+                select: {
+                    id: true,  // existence만 체크
+                }
+            })
+
+            if (ok) {
+                return true;
+            }
+            return false;
         }
     },
 
