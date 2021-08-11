e4fd08e920f8252b7557f8f209c861d3b0acbe43
menuin <menuin@hanyang.ac.kr>
2021-08-12 03:30:57 +0900

    update from native - #15.10

--- photos/seeFeed/seeFeed.resolvers.js
+++ photos/seeFeed/seeFeed.resolvers.js
@@ -3,7 +3,9 @@ import { protectedResolver } from "../../users/users.utils";
 
 export default {
     Query: {
-        seeFeed: protectedResolver((_, __, { loggedInUser }) => client.photo.findMany({
+        seeFeed: protectedResolver((_, { offset }, { loggedInUser }) => client.photo.findMany({
+            take: 2,
+            skip: offset,
             where: {
                 OR: [
                     {
